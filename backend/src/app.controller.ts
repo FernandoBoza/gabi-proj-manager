a55ff7eeaa1ed159c46b46109ff75c2922a89f19
fernandoboza <hi@fernandoboza.com@users.noreply.github.com>
2021-02-16 12:22:40 -0500

    added role gaurds to App Controller

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Roles } from './auth/roles/roles.decorator';
+import { Role } from './auth/roles/roles.enum';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Roles(Role.Admin)
   getHello(): string {
     return this.appService.getHello();
   }
